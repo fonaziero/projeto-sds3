ef9a8ccf748cbb03f61e24b2395f6060ca1ee930
Victor <victorcarnevale@yahoo.com.br>
2021-05-03 16:46:50 -0300

    Projetos criados

---
+++ backend/src/test/java/com/fonaziero/dsvendas/DsvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fonaziero.dsvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
