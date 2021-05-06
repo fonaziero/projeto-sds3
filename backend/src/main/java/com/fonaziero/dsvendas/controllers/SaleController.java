62f8be858a6d48abac4c4bdc803015e0ffd26bd9
Victor <victorcarnevale@yahoo.com.br>
2021-05-06 19:10:34 -0300

    Paginacao

--- backend/src/main/java/com/fonaziero/dsvendas/controllers/SaleController.java
+++ backend/src/main/java/com/fonaziero/dsvendas/controllers/SaleController.java
@@ -1,28 +1,26 @@
 package com.fonaziero.dsvendas.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.fonaziero.dsvendas.dto.SaleDTO;
-import com.fonaziero.dsvendas.service.SaleService;
-
-
+import com.fonaziero.dsvendas.dto.SellerDTO;
+import com.fonaziero.dsvendas.service.SellerService;
 
 @RestController
-@RequestMapping(value = "/sales")
-public class SellerController {
+@RequestMapping(value = "/sellers")
+public class SaleController {
 
 	@Autowired
-	private SaleService service;
+	private SellerService service;
 	
 	@GetMapping
-	public ResponseEntity<Page<SaleDTO>> findAll(Pageable pageable) {
-		Page<SaleDTO> list = service.findall(pageable);
+	public ResponseEntity<List<SellerDTO>> findAll() {
+		List<SellerDTO> list = service.findall();
 		
 		return ResponseEntity.ok(list);	
 	}
---
+++ backend/src/main/java/com/fonaziero/dsvendas/repository/SaleRepository.java
@@ -0,0 +1,10 @@
+package com.fonaziero.dsvendas.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.fonaziero.dsvendas.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long>{
+
+}
+	
